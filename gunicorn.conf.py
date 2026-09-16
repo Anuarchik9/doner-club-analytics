46ff048ec7de71f841d7b3e65371753ea14d9db3
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 15:09:34 +0500

    Install sales channel analytics filter

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,6 @@
 def post_worker_init(worker):
     from dashboard_auth import install_auth
+    from sales_channel import install_sales_channel
 
     install_auth(worker.wsgi)
+    install_sales_channel(worker.wsgi)
