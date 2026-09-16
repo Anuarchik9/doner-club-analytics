72caecd43287c4428d7634a303c37d2161f7508e
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 16:08:18 +0500

    Install dashboard speed patch

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -12,11 +12,13 @@ def post_worker_init(worker):
     from dashboard_auth import install_auth
     from sales_channel import install_sales_channel
     from team_meal import install_team_meal
+    from speed_patch import install_speed_patch
     from chart_hover import install_chart_hover
     from preset_controls import install_preset_controls
 
     install_auth(worker.wsgi)
     install_sales_channel(worker.wsgi)
     install_team_meal(worker.wsgi)
+    install_speed_patch(worker.wsgi)
     install_chart_hover(worker.wsgi)
     install_preset_controls(worker.wsgi)
