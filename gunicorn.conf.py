b6deed419527202ec71b92e785b903b7214fa3a5
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 21:19:52 +0500

    Enable revision book-balance accuracy backend

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -28,10 +28,10 @@ def post_worker_init(worker):
     from report_final_polish import install_report_final_polish
     from navigation_shell import install_navigation_shell
     from revisions_ui import install_revisions_ui
-    from revisions_data_v6 import install_revisions_data_v6
+    from revisions_data_v7 import install_revisions_data_v7
 
     install_auth(worker.wsgi)
-    install_revisions_data_v6(worker.wsgi)
+    install_revisions_data_v7(worker.wsgi)
     # The first registered after-request extension is injected last into the HTML.
     install_revisions_ui(worker.wsgi)
     install_navigation_shell(worker.wsgi)
