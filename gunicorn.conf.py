9c5c3e6d3dae9672d44667084b9c4be329ccc329
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 18:02:11 +0500

    Install final UI fixes for logo and sales channel icons

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -25,9 +25,11 @@ def post_worker_init(worker):
     from ui_upgrade import install_ui_upgrade
     from trend_label_patch import install_trend_label_patch
     from compact_layout import install_compact_layout
+    from final_fixes import install_final_fixes
 
     install_auth(worker.wsgi)
     # The first registered after-request extension is injected last into the HTML.
+    install_final_fixes(worker.wsgi)
     install_compact_layout(worker.wsgi)
     install_trend_label_patch(worker.wsgi)
     install_ui_upgrade(worker.wsgi)
