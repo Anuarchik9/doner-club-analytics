6053c52a4cee281fbf37f54504de182a3410035d
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 22:53:11 +0500

    Load hard modal close fix and bust revision UI caches

--- revisions_ui.py
+++ revisions_ui.py
@@ -22,11 +22,12 @@ def install_revisions_ui(app):
                     '<script src="/static/revision-balance.js?v=20260916-1"></script>'
                     '<script src="/static/revision-audit.js?v=20260916-1"></script>'
                     '<script src="/static/revision-management.js?v=20260916-1"></script>'
-                    '<script src="/static/revision-product-drilldown.js?v=20260916-1"></script>'
-                    '<script src="/static/revision-product-diagnostic.js?v=20260916-1"></script>'
-                    '<script src="/static/revision-product-modal-large.js?v=20260916-1"></script>'
-                    '<script src="/static/revision-product-modal-fix.js?v=20260916-1"></script>'
-                    '<script src="/static/revision-investigation.js?v=20260916-1"></script>'
+                    '<script src="/static/revision-product-drilldown.js?v=20260916-2"></script>'
+                    '<script src="/static/revision-product-diagnostic.js?v=20260916-2"></script>'
+                    '<script src="/static/revision-product-modal-large.js?v=20260916-2"></script>'
+                    '<script src="/static/revision-product-modal-fix.js?v=20260916-2"></script>'
+                    '<script src="/static/revision-investigation.js?v=20260916-2"></script>'
+                    '<script src="/static/revision-product-close-hardfix.js?v=20260916-1"></script>'
                     '<script src="/static/revisions-ui.js?v=20260916-6"></script>'
                 )
                 if "/static/revision-details.js" not in body and "</body>" in body:
