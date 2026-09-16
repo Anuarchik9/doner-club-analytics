265a9ee33f51fa77d86a4d037b7c9fbda3382cff
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 21:04:15 +0500

    Load revision trend insights UI

--- revisions_ui.py
+++ revisions_ui.py
@@ -18,6 +18,7 @@ def install_revisions_ui(app):
                 body = response.get_data(as_text=True)
                 tags = (
                     '<script src="/static/revision-details.js?v=20260916-2"></script>'
+                    '<script src="/static/revision-insights.js?v=20260916-1"></script>'
                     '<script src="/static/revisions-ui.js?v=20260916-6"></script>'
                 )
                 if "/static/revision-details.js" not in body and "</body>" in body:
