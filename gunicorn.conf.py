c8e5cb4ce9267f43b36e0179fa2960ac07f93dfd
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 15:12:51 +0500

    Enable revenue chart hover tooltips

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,8 @@
 def post_worker_init(worker):
     from dashboard_auth import install_auth
     from sales_channel import install_sales_channel
+    from chart_hover import install_chart_hover
 
     install_auth(worker.wsgi)
     install_sales_channel(worker.wsgi)
+    install_chart_hover(worker.wsgi)
