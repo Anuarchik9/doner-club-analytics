ae57f9eddd095b024c4ea00299248dd5c0e3f9c6
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 15:37:55 +0500

    Enable quick presets for previous calendar months

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,8 +3,10 @@ def post_worker_init(worker):
     from sales_channel import install_sales_channel
     from team_meal import install_team_meal
     from chart_hover import install_chart_hover
+    from preset_controls import install_preset_controls
 
     install_auth(worker.wsgi)
     install_sales_channel(worker.wsgi)
     install_team_meal(worker.wsgi)
     install_chart_hover(worker.wsgi)
+    install_preset_controls(worker.wsgi)
