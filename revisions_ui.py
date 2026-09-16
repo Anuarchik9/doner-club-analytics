503ad2990f8ea890990ea5d4a551ec21bc8ae358
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 23:01:42 +0500

    Use single revision modal close controller

--- revisions_ui.py
+++ revisions_ui.py
@@ -25,9 +25,8 @@ def install_revisions_ui(app):
                     '<script src="/static/revision-product-drilldown.js?v=20260916-2"></script>'
                     '<script src="/static/revision-product-diagnostic.js?v=20260916-2"></script>'
                     '<script src="/static/revision-product-modal-large.js?v=20260916-2"></script>'
-                    '<script src="/static/revision-product-modal-fix.js?v=20260916-2"></script>'
                     '<script src="/static/revision-investigation.js?v=20260916-2"></script>'
-                    '<script src="/static/revision-product-close-hardfix.js?v=20260916-1"></script>'
+                    '<script src="/static/revision-product-close-hardfix.js?v=20260916-2"></script>'
                     '<script src="/static/revisions-ui.js?v=20260916-6"></script>'
                 )
                 if "/static/revision-details.js" not in body and "</body>" in body:
