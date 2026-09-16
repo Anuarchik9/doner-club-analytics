93f0b48c86075dc6389ff4986f978e514853e305
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-17 01:04:13 +0500

    Refresh conditional period notes script

--- period_notes.py
+++ period_notes.py
@@ -16,8 +16,8 @@ def install_period_notes(app):
             try:
                 response.direct_passthrough = False
                 body = response.get_data(as_text=True)
-                tag = '<script src="/static/period-notes.js"></script>'
-                if tag not in body and "</body>" in body:
+                tag = '<script src="/static/period-notes.js?v=20260917-2"></script>'
+                if "period-notes.js" not in body and "</body>" in body:
                     response.set_data(body.replace("</body>", tag + "</body>", 1))
                     response.headers.pop("Content-Length", None)
             except Exception:
