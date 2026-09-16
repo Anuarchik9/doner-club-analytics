6c8efcf92e533584e47f902827269d413dce8629
Anuarchik9 <52879914+Anuarchik9@users.noreply.github.com>
2026-09-16 23:48:47 +0500

    Bust cache for custom date picker

--- wide_period_click.py
+++ wide_period_click.py
@@ -16,7 +16,7 @@ def install_wide_period_click(app):
             try:
                 response.direct_passthrough = False
                 body = response.get_data(as_text=True)
-                tag = '<script src="/static/wide-period-click.js?v=20260916-2"></script>'
+                tag = '<script src="/static/wide-period-click.js?v=20260916-3"></script>'
                 if "wide-period-click.js" not in body and "</body>" in body:
                     response.set_data(body.replace("</body>", tag + "</body>", 1))
                     response.headers.pop("Content-Length", None)
